b1e2a7ab01a7d604ccce27ce55db4855edcaf60c
michaelthoff <michaelthoff@gmail.com>
2016-05-28 18:10:36 -0700

    Update MazeGame_Factory.py

--- python/MazeGame_Factory.py
+++ python/MazeGame_Factory.py
@@ -26,18 +26,9 @@ class Maze:
 
 class HauntedRoom(Room): pass
 
-
-class HauntedWall(Wall): pass
-
-
 class HauntedMaze(Maze): pass
 
-
-class EnchantedRoom(Room): pass
-
-
 class EnchantedWall(Wall): pass
-
 
 class EnchantedMaze(Maze): pass
 
