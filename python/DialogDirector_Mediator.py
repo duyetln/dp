14431b7f82a5b550aa67dde5bed41fd295efb91e
michaelthoff <michaelthoff@gmail.com>
2016-06-03 11:58:16 -0700

    Update DialogDirector_Mediator.py

--- python/DialogDirector_Mediator.py
+++ python/DialogDirector_Mediator.py
@@ -2,24 +2,12 @@
 
 class DialogDirector:
 
-    def __init__(self):
-         pass
-
-    def ShowDialog(self):
-        # show dialog
-        pass
-
     def WidgetChanged(self, Widget):
         # do a thing based on widget change
         pass
 
-    def CreateWidgets(self):
-        pass
-
 
 class Widget:
-
-    DialogDirector = None
 
     def __init__(self, DialogDirector):
         self.DialogDirector = DialogDirector
@@ -27,62 +15,41 @@ class Widget:
     def Changed(self):
         self.DialogDirector.WidgetChanged(self)
 
-    def HandleMouse(self, MouseEvent, Event):
-        # respond to user's mouse input
-        pass
 
 class ListBox(Widget):
+    
+    def SetList(self):
+        print "set list"
 
-    def GetSelection(self):
-        char = None
-        return char
-
-    def HandleMouse(self, MouseEvent, Event):
-        pass
 
 class EntryField(Widget):
 
-    def SetText(self, text):
-        # set text
-        pass
+    def SetText(self):
+        print "set text"
 
-    def GetText(self):
-        # return selected text
-        char = None
-        return char
-
-    def HandleMouse(self, MouseEvent, Event):
-        pass
 
 class Button(Widget):
 
-    def SetText(self, text):
-        # set text
-        pass
-
-    def HandleMouse(self, MouseEvent, Event):
-        pass
+    def Click(self):
+        print "click"
 
 
 class FontDialogDirector(DialogDirector):
 
-    def __init__(self):
-        DialogDirector.__init__(self)
-
-    def CreateWidgets(self):
-        self.ok = Button(self)
-        self.cancel = Button(self)
-        self.fontList = ListBox(self)
-        self.fontName = EntryField(self)
+    ok = Button(self)
+    cancel = Button(self)
+    box = ListBox(self)
+    entry = EntryField(self)
 
     def WidgetChanged(self, changedWidget):
-        if changedWidget == self.fontList:
-            self.fontName.SetText(self.fontList.GetSelection())
-        elif changedWidget == self.ok:
-            # apply font change, dismiss dialog
-            pass
+        if changedWidget == self.ok:
+            entry.SetList()
         elif changedWidget == self.cancel:
-            # dismiss dialog
-            pass
+            box.SetList()
 
 
+director = new FrontDialogDirector()
+ok  = director.ok
+cancel = director.cancel
+ok.click
+cancel.click
