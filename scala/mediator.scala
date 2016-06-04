f3a4e44edfeee7295de7735228e1d4aaa5143084
Duyet Nguyen <duyetlnguyen@gmail.com>
2016-06-03 22:42:48 -0700

    renaming to avoid confusion

--- scala/mediator.scala
+++ scala/mediator.scala
@@ -1,33 +1,33 @@
-abstract class DialogDirector {
+abstract class WidgetMediator {
   def widgetChanged(widget : AnyRef)
 }
 
-abstract class Widget(val director : DialogDirector) {
+abstract class Widget(val director : WidgetMediator) {
   def changed {
     director.widgetChanged(this)
   }
 }
 
-class ListBox(director : DialogDirector) extends Widget(director) {
+class ListBox(director : WidgetMediator) extends Widget(director) {
   def setList {
     println("setList")
   }
 }
 
-class EntryField(director : DialogDirector) extends Widget(director) {
+class EntryField(director : WidgetMediator) extends Widget(director) {
   def setText {
     println("setText")
   }
 }
 
-class Button(director : DialogDirector) extends Widget(director) {
+class Button(director : WidgetMediator) extends Widget(director) {
   def click {
     println("click")
     changed
   }
 }
 
-class FrontDialogDirector extends DialogDirector {
+class FrontDialogMediator extends WidgetMediator {
   val ok = new Button(this)
   val cancel = new Button(this)
   val box = new ListBox(this)
@@ -41,7 +41,7 @@ class FrontDialogDirector extends DialogDirector {
   }
 }
 
-val director = new FrontDialogDirector
+val director = new FrontDialogMediator
 val ok = director.ok
 val cancel = director.cancel
 ok.click
